39379291568a88f7108cabc8aa8d7a7975d0ad27
Andy Hua <user.andy.hua@gmail.com>
2022-10-18 23:18:06 -0700

    works

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,56 +1,121 @@
-import { Input, Button } from "reactstrap";
+import { Input, Button, Card } from "reactstrap";
 import "./Filter.scss";
-import Items from "../Items/Items";
+import "./Items.scss";
 import { useState } from "react";
 
 const Filter = ({ menu }) => {
     const [data, setData] = useState(menu);
+    const [min, setMin] = useState(null);
+    const [max, setMax] = useState(null);
+    const [show, setShow] = useState(false);
 
     const handleReset = () => {
         setData(menu);
+        setShow(false);
     };
 
-    const handleSearch = (e) => {
-        let newData = menu.filter((x) => x.title.search(e.target.value) > -1);
+    const handleSearch = e => {
+        let newData = menu.filter(
+            x => x.title.toLowerCase().search(e.target.value.toLowerCase()) > -1
+        );
+        setData(newData.length ? newData : menu);
+        setShow(newData.length ? false : true);
+    };
+
+    const handleMin = e => {
+        let newValue = e.target.value;
+        setMin(newValue);
+        let newData = menu.filter(x => x.price >= min && x.price <= max);
         setData(newData);
-        console.log(newData);
+        setShow(newData.length ? false : true);
     };
 
-    const handleFilter = (e) => {};
+    const handleMax = e => {
+        let newValue = e.target.value;
+        setMax(newValue);
+        let newData = menu.filter(x => x.price >= min && x.price <= max);
+        setData(newData);
+        setShow(newData.length ? false : true);
+    };
+
+    const handleCat = e => {
+        let newData = data.filter(({ category }) => e.target.value === category.name);
+        if (min && max) {
+            newData = newData.filter(({ price }) => price >= min && price <= max);
+        }
+        setData(newData.length ? newData : menu);
+    };
 
     return (
         <>
             <form className="Filter">
                 <Input
-                    onChange={(e) => handleSearch(e)}
+                    onChange={e => handleSearch(e)}
                     placeholder="Search"
                     className="search my-4"
                 />
                 <p>Price Filter</p>
                 <div className="min-max">
                     <Input
-                        onChange={(e) => handleFilter(e)}
+                        onChange={e => handleMin(e)}
                         placeholder="min"
                         type="number"
                         className="num"
+                        min={0}
                     />
                     <Input
-                        onChange={(e) => handleFilter(e)}
+                        onChange={e => handleMax(e)}
                         placeholder="max"
                         type="number"
                         className="num "
+                        min={0}
                     />
-                    <Button
-                        type="reset"
-                        onClick={handleReset}
-                        className="reset"
-                        outline
-                    >
+                    <Button type="reset" onClick={handleReset} className="reset" outline>
                         Reset
                     </Button>
                 </div>
             </form>
-            <Items menu={data} />
+
+            <div className="main d-flex flex-column align-items-center py-5">
+                <div className="categories">
+                    {menu
+                        .reduce(
+                            (acc, { category }) => {
+                                return !acc.includes(category.name) ? [...acc, category.name] : acc;
+                            },
+                            ["All"]
+                        )
+                        .map((x, i) => {
+                            return (
+                                <Button
+                                    key={i}
+                                    value={x}
+                                    onClick={e => {
+                                        handleCat(e);
+                                    }}
+                                    className="mx-3"
+                                >
+                                    {x}
+                                </Button>
+                            );
+                        })}
+                </div>
+                <h3 style={{ display: show ? "block" : "none" }}>No Results.</h3>
+                <div className="Items m-4">
+                    {data.map((item, i) => {
+                        let { images, title, price } = item;
+                        return (
+                            <Card key={i} className="item-card">
+                                <img className="card-img" src={images[0]} alt={title} />
+                                <div className="desc p-2">
+                                    <p className="px-3">{title}</p>
+                                    <p className="price px-3"> ${price}</p>
+                                </div>
+                            </Card>
+                        );
+                    })}
+                </div>
+            </div>
         </>
     );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import "./Header.scss";
 const Header = () => {
     return (
         <div className="Header">
-            <h1 className="my-4">Shopping</h1>
+            <h1 className="my-4">Shop</h1>
             <div className="underline"></div>
         </div>
     );
